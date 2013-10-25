a210620bd8d7be144c5897c0bda66402afe35e96
Andrew Hampe <andrew.hampe@gmail.com>
2013-10-25 16:58:32 -0500

    Added detection for expired access grants

--- hawkenapi/exceptions.py
+++ hawkenapi/exceptions.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # API Exceptions
 
+import re
+
+
 class ApiException(Exception):
     def __init__(self, message, code):
         self.message = message
@@ -9,7 +12,16 @@ class ApiException(Exception):
 
 
 class NotAuthorized(ApiException):
-    pass
+    _re_expired = re.compile(r"Invalid Access Grant:\s*\(exp\([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{7}Z\) <= now\([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{7}Z\)\)")
+
+    def __getattr__(self, key):
+        if key == "expired":
+            return self._expired()
+        else:
+            raise AttributeError
+
+    def _expired(self):
+        return self._re_expired.match(self.message) is not None
 
 
 class InternalServerError(ApiException):
