9a905e7cb32cd72eccfe486f010c1064c1b59e23
Andrew Hampe <andrew.hampe@gmail.com>
2013-12-26 14:23:50 -0600

    Version bump to 0.1.4.1

--- hawkenapi/__init__.py
+++ hawkenapi/__init__.py
@@ -3,4 +3,4 @@
 # Pull in the client
 from hawkenapi.client import Client
 
-__version__ = "0.1.4"
+__version__ = "0.1.4.1"
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name="hawkenapi",
-      version="0.1.4",
+      version="0.1.4.1",
       description="Hawken API Client Library",
       author="Andrew Hampe",
       author_email="andrew.hampe@gmail.com",
