14e02630889fdea495fc4598ea6187b98b26f8d7
Andrew Hampe <andrew.hampe@gmail.com>
2014-03-31 15:02:21 -0500

    Version bump to 0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name="hawkenapi",
-      version="0.4",
+      version="0.5",
       description="Hawken API Client Library",
       author="Andrew Hampe",
       author_email="andrew.hampe@gmail.com",
